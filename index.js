c6b26637e736f2edae82b6a644193b9f10a2620a
James <gacutanjms@gmail.com>
2022-09-01 15:10:42 +0800

    Added a function for comp choice

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// made the choices a global var
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
 
 // Make the button click-able
 
@@ -8,13 +12,10 @@ const btnScissors = document.querySelector('.scissors');
 // Add a function to listen to a click event and then assign a value for that button
 function getPlayerChoice(e) {
     if (e.target === btnRock) {
-        const rock = 'rock';
         return rock;
     } else if (e.target === btnPaper){
-        const paper = 'paper';
         return paper;
     } else if (e.target === btnScissors) {
-        const scissors = 'scissors';
         return scissors;
     }
 }
@@ -26,4 +27,16 @@ btnScissors.addEventListener('click', getPlayerChoice);
 
 // Add a function for computer choice
 
+function getComputerChoice(max) {
+    let choice = Math.floor(Math.random() * max + 1);
+    // Set the 3 numbers to Rock, Paper and Scissor
+    if (choice === 1 ){
+        return rock;
+    } else if (choice === 2) {
+        return paper;
+    } else if (choice === 3) {
+        return scissors;
+    }
+}
 
+console.log(getComputerChoice(3));
