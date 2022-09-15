ea90f37d4dd75c8b301d20980349e6c79cfa6f6e
James <gacutanjms@gmail.com>
2022-09-15 15:48:35 +0800

    Created a function for starting the game

--- index.js
+++ index.js
@@ -1,42 +1,126 @@
-// made the choices a global var
-const rock = 'rock';
-const paper = 'paper';
-const scissors = 'scissors';
+// global variable for choices 
 
-// Make the button click-able
+var rock = 'Rock';
+var paper = 'Paper';
+var scissors = 'Scissors';
 
-const btnRock = document.querySelector('.rock');
-const btnPaper = document.querySelector('.paper');
-const btnScissors = document.querySelector('.scissors');
+// Score container 
+let pScore = 0;
+let cmpScore = 0;
 
-// Add a function to listen to a click event and then assign a value for that button
-function getPlayerChoice(e) {
-    if (e.target === btnRock) {
-        return rock;
-    } else if (e.target === btnPaper){
-        return paper;
-    } else if (e.target === btnScissors) {
-        return scissors;
-    }
-}
-
-btnRock.addEventListener('click', getPlayerChoice);
-btnPaper.addEventListener('click', getPlayerChoice);
-btnScissors.addEventListener('click', getPlayerChoice);
-
-
-// Add a function for computer choice
-
-function getComputerChoice(max) {
-    let choice = Math.floor(Math.random() * max + 1);
-    // Set the 3 numbers to Rock, Paper and Scissor
-    if (choice === 1 ){
-        return rock;
+// Choices
+let computerChoice;
+// function for computer choice 
+function getComputerChoice(n) {
+    let choice = Math.floor(Math.random() * n + 1);
+    // Set 3 numbers to Rock, Paper and Scissors
+    if (choice === 1) {
+        return rock;   
     } else if (choice === 2) {
         return paper;
-    } else if (choice === 3) {
-        return scissors;
+    } else {
+        return scissors; 
     }
 }
 
-console.log(getComputerChoice(3));
+// function for getting player choice
+function getPlayerChoice(e) {
+
+    let playerChoice;
+
+    if (e.target === btnRock) {
+        btnRock.textContent = rock;
+        return playerChoice = btnRock.textContent;
+    } else if (e.target === btnPaper) {
+        btnPaper.textContent = paper;
+        return playerChoice = btnPaper.textContent;
+    } else if (e.target === btnScissors) {
+        btnScissors.textContent = scissors;
+        return  playerChoice = btnScissors.textContent;
+    }
+}
+
+// Show computer choice in html
+// function showLabel (){
+//     computerLabel = document.querySelector('.computer-label')
+
+//     compPara = document.createElement('p');
+//     computerLabel.appendChild(compPara);
+
+//     compText = document.createTextNode(`Computer have chosen ${computerChoice}`);
+//     compPara.appendChild(compText);
+// }
+
+
+// function for player choice 
+
+// let choices = document.querySelector('.choices');
+
+
+
+let btnRock = document.querySelector('.rock');
+let btnPaper = document.querySelector('.paper');
+let btnScissors = document.querySelector('.scissors');
+
+
+btnRock.addEventListener('click' , () => {
+    playerChoice = btnRock.textContent;
+    computerChoice = getComputerChoice(3);
+
+    console.log(`Computer : ${computerChoice}`);
+    console.log(`Player: ${playerChoice}`);
+    startRound(playerChoice, computerChoice);
+});
+
+btnPaper.addEventListener('click' , () => {
+    playerChoice = btnPaper.textContent;
+    computerChoice = getComputerChoice(3);
+
+    console.log(`Computer : ${computerChoice}`);
+    console.log(`Player: ${playerChoice}`);
+    startRound(playerChoice, computerChoice);
+});
+
+btnScissors.addEventListener('click' , () => {
+    playerChoice = btnScissors.textContent;
+    computerChoice = getComputerChoice(3);
+
+    console.log(`Computer : ${computerChoice}`);
+    console.log(`Player: ${playerChoice}`);
+    startRound(playerChoice, computerChoice);
+});
+
+
+    
+
+// function for starting the game
+
+function startRound(playerChoice, computerChoice){
+
+    if (playerChoice === computerChoice){
+        console.log('DRAW');
+    } else if (playerChoice === rock & computerChoice === scissors) {
+        pScore += 1;
+        console.log('Player Wins! Rock beats Scissors');
+    } else if (playerChoice === paper & computerChoice === rock) {
+        pScore += 1;
+        console.log('Player Wins! Paper beats Rock');
+    } else if (playerChoice === scissors & computerChoice === paper) {
+        pScore += 1;
+        console.log('Player Wins! Scissors beats Paper');
+    } else if (computerChoice === rock & playerChoice === scissors) {
+        cmpScore += 1;
+        console.log('Computer Wins! Rock beats Scissors');
+    } else if (computerChoice === paper & playerChoice === rock) {
+        cmpScore += 1;
+        console.log('Computer Wins! Paper beats Rock');
+    } else if (computerChoice === scissors & playerChoice === paper) {
+        cmpScore += 1;
+        console.log('Computer Wins! Scissors beats Paper');
+    } 
+        
+        console.log(pScore);
+        console.log(cmpScore);
+    }
+
+
